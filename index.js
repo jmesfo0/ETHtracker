d9655ead9ad2716585dc61cf16cb46a4d65150ff
James Y <42681518+jmesfo0@users.noreply.github.com>
2022-06-14 20:39:50 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,46 @@
+import { Client, Intents } from "discord.js";
+import axios from "axios";
+import express from "express";
+
+const server = express();
+
+server.get('/', function (req, res) {
+  res.send('BTCTracker')
+});
+
+function keepAlive() {
+  server.listen(3000, () => {
+    console.log("Server is ready.");
+  });
+}
+
+const url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd";
+let lastPrice;
+
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS],
+});
+
+const BOT_ID = process.env["BOT_ID"];
+const BOT_TOKEN = process.env["BOT_TOKEN"];
+
+client.once("ready", () => {
+  console.log("We on . . .");
+
+  const SERVER_ID = process.env["SERVER_ID"];
+  const guild = client.guilds.cache.get(SERVER_ID);
+  
+  setInterval(async () => {
+    getPrice();
+    client.user.setActivity("BTC: $" + lastPrice, { type: "WATCHING" }).catch(console.error);
+  }, 5000);
+});
+
+function getPrice() {
+  axios.get(url).then((response) => {
+    lastPrice = response.data.bitcoin.usd;
+  });
+}
+
+keepAlive();
+client.login(BOT_TOKEN);
